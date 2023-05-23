fb8a17486b795ae2ccad265a7c921d2174d7d21b
pratu52kule <132321426+pratu52kule@users.noreply.github.com>
2023-05-23 17:18:14 +0530

    javascript

---
+++ day-20.js
@@ -0,0 +1,23 @@
+var myname =`pratiksha`;
+var surname ="Bawankule";
+var mylocation='amravti';
+var myinstitude="i-rise";
+console.log(myname,surname,mylocation,myinstitude);
+var num1=20;
+var num2=5;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+var N =100;
+console.log(N=='100');
+console.log(N==='100');
+if(N>=100){
+    console.log('N value is greter or equal to 100');
+}
+else if(N>50){
+    console.log('N value is greter than 50');
+}
+else{
+    console.log("N value is not greator than 50 or 100");
+}
