e41b1348a31e5d473b3d571a68721ec76586df3d
pratu52kule <132321426+pratu52kule@users.noreply.github.com>
2023-05-30 08:16:01 +0530

    update

---
+++ day-25.js
@@ -0,0 +1,26 @@
+//given an array find which number are greater than given target
+var array =[5,11,65,27,3,7,9,15,54,52,77,121];
+var target =15;
+function findlargenumber(array,target){
+    for(var i=0;i<array.length;i++){
+        if(array[i]>target){
+            console.log(array[i])
+        }
+    }
+}
+findlargenumber(array,target);
+var myname='pratiksha';
+myname='irise';
+var myname='hello'
+console.log(myname)
+
+
+let change='xyz';
+change=`pihu`;
+let change=`sam`;
+console.log(change)
+
+const anyname=`swaraj`;
+//anyname='shiv';
+//const anyname='radha';
+console.log(anyname);
