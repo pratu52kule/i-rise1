44efb8eaa6bffa9c055ad6a38bf6379d7860a5e5
pratu52kule <132321426+pratu52kule@users.noreply.github.com>
2023-06-01 17:59:33 +0530

    update

--- day-25.js
+++ day-25.js
@@ -2,25 +2,28 @@
 var array =[5,11,65,27,3,7,9,15,54,52,77,121];
 var target =15;
 function findlargenumber(array,target){
+    var count=0;
     for(var i=0;i<array.length;i++){
         if(array[i]>target){
-            console.log(array[i])
+            //console.log(array[i])
+            count++;
         }
     }
+    console.log(count)
 }
 findlargenumber(array,target);
-var myname='pratiksha';
-myname='irise';
-var myname='hello'
-console.log(myname)
+//var myname='pratiksha';
+//myname='irise';
+//var myname='hello'
+//console.log(myname)
 
 
-let change='xyz';
-change=`pihu`;
-let change=`sam`;
-console.log(change)
+//let change='xyz';
+//change=`pihu`;
+//let change=`sam`;
+//console.log(change)
 
-const anyname=`swaraj`;
+//const anyname=`swaraj`;
 //anyname='shiv';
 //const anyname='radha';
-console.log(anyname);
+//console.log(anyname);
---
+++ object.js
@@ -0,0 +1,5 @@
+//object-multiple data with multiple datatype
+var myobject={students:100,studentlist:["vijay","vaibhaV","irise"] ,lectures:["js","java",92]}
+console.log(myobject.studentlist)
+var myarray=myobject.lectures;
+console.log(myarray)
